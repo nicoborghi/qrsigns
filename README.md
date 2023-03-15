c8cc2c4a5081f59ece02d3836c1fd2e0f2b7980f
Nicola Borghi <nicoborghi@outlook.it>
2023-03-15 14:32:27 +0100

    project completed

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # qrsigns
-Script that generates multiple images with associated QR code from a .csv file
+Scripts to generate multiple images with associated QR code using data stored in a .csv file
 
-I used this script to generate tags with QR code and associate url for the botanical garden of San Giovanni in Persiceto (BO)
+## `qrsigns_SGP.py`
+This script was used to generate ~200 garden signs for the Botanical Garden in San Giovanni in Persiceto (BO). This project is in collaboration with local WWF volunteers who installed the QR garden signs in February 2023.
+
+Here is an example (with its practical application):
+
+<img src="images/example_SGP.png" width=500></img>
+<img src="images/example_SGP_photo.jpg" width=500></img>
--- qrsigns_SGP.py
+++ qrsigns_SGP.py
@@ -1,14 +1,9 @@
 ###################################################
-# This is a script to create multiple QR labels
-# with some text and a logo image from a csv file
-#
-# I used this script to generate ~200 garden signs
-# in .png format for the Botanical Garden at
-# San Giovanni in Persiceto (BO)
+# This is a script to create multiple images with
+# QR code, some text, and logos from a .csv file
 #
 # author: Nicola Borghi
 # e-mail: nicoborghi [at] outlook.com
-#
 ###################################################
 
 # Import libraries
@@ -19,35 +14,39 @@ import qrcode
 from pandas import read_csv 
 from PIL import Image, ImageFont, ImageChops, ImageDraw
 
-
-os.chdir('/home/nic/progetti/orto/')
 # Directory definition
 dir_home = os.getcwd()
-dir_imm = dir_home+'/signs/'
-dir_qr = dir_home+'/qr/'
+dir_out  = dir_home+'/signs/'
+dir_qr   = None
 
-# Filename definition
-filecsv='input.csv'
+# Fonts definition
+fontreg  = ImageFont.truetype("fonts/HardingText-Regular-Web.ttf", 95)
+fontbold = ImageFont.truetype("fonts/HardingText-Bold-Web.ttf", 100)
+fontit   = ImageFont.truetype("fonts/HardingText-RegularItalic-Web.ttf", 80)
+
+# Input file definition
+filecsv='data/orto_botanico_san_giovanni_persiceto.csv'
+
 
 # Creation of the directories
-if not os.path.exists(dir_imm):
-	os.mkdir(dir_imm)
-	print("Directory " , dir_imm ,  " created ")
+if not os.path.exists(dir_out):
+	os.mkdir(dir_out)
+	print("Directory " , dir_out ,  " created ")
 else:
-	print("Directory " , dir_imm ,  " already existing")
-if not os.path.exists(dir_qr):
-	os.mkdir(dir_qr)
-	print("Directory " , dir_qr ,  " created ")
-else:
-	print("Directory " , dir_qr ,  " already existing")
+	print("Directory " , dir_out ,  " already existing")
+	
+if dir_qr is not None:
+    if not os.path.exists(dir_qr):
+        os.mkdir(dir_qr)
+        print("Directory " , dir_qr ,  " created ")
+    else:
+        print("Directory " , dir_qr ,  " already existing")
 
 # Check for the existence of the input '.csv' file
 if not os.path.isfile(filecsv):
     sys.exit("\nError: File {:s} not found in the current working directory. Have a nice day!\n".format(filecsv))
 
-
 df = read_csv(filecsv) 
-#print (df['URL'])    #sanity check
 
 
 ####################
@@ -57,33 +56,28 @@ df = read_csv(filecsv)
 ####################
 
 # Image Components
-logo = Image.open('logo.jpg')
-wwf = Image.open('wwf.png')
-mct = Image.open('mct.png')
-logo = logo.resize((375, 350), Image.ANTIALIAS)
-mct = mct.resize((467, 350), Image.ANTIALIAS)
-wwf = wwf.resize((236, 350), Image.ANTIALIAS)
-bkg = Image.open('background.png')
+logo = Image.open('images/logo.jpg')
+wwf = Image.open('images/wwf.png')
+mct = Image.open('images/mct.png')
+bkg = Image.open('images/background.png')
 
-
-# Fonts
-fontreg = ImageFont.truetype("/home/nic/.local/share/fonts/Unknown Vendor/TrueType/Harding Text Web/Harding_Text_Web_Regular.ttf", 95)
-fontbold = ImageFont.truetype("/home/nic/.local/share/fonts/Commercial Type/TrueType/Harding Text Web/Harding_Text_Web_Bold.ttf", 100)
-fontit = ImageFont.truetype("/home/nic/.local/share/fonts/Unknown Vendor/TrueType/Harding Text Web/Harding_Text_Web_Italic.ttf", 80)
-
+logo = logo.resize((375, 350), Image.Resampling.LANCZOS)
+mct = mct.resize((467, 350), Image.Resampling.LANCZOS)
+wwf = wwf.resize((236, 350), Image.Resampling.LANCZOS)
 
 
 for i in range(len(df)):
     #QR Code generation
     print("Generating: {:s}".format(df['URL'][i]))
     qr = qrcode.make(df['URL'][i])
-    qrfile=dir_qr+"{:04d}.png".format(i)
-    qr.save(qrfile)
+    
+    if dir_qr is not None:
+        qr.save(dir_qr+"{:04d}.png".format(i))
 
     # Sign design: qr, logo and background
     sizeimm=(1772, 1181)
     imm = Image.new(mode = "RGB", size = sizeimm, color = (255, 255, 255))
-    qr = qr.resize((570, 570), Image.ANTIALIAS)
+    qr = qr.resize((570, 570), Image.Resampling.LANCZOS)
     imm.paste(qr,  (20, 20))
     imm.paste(mct,  (550, 100))
     imm.paste(logo,  (970, 90))
@@ -108,6 +102,6 @@ for i in range(len(df)):
 
 
     # Save Images
-    immfile=dir_imm+"{:04d}.png".format(i)
+    immfile=dir_out+"{:04d}.png".format(i)
     imm.save(immfile, "PNG", quality=100)
     imm.close()
